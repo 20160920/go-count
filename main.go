bf1fc5922b152c26d3df4aec1fce450c1fb5dc5a
qihuang <941089419@qq.com>
2021-12-01 09:44:11 +0800

    push

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 
 type SafeCounter struct {
 	mu sync.Mutex
+<<<<<<< HEAD
 	v map[string]int
 }
 
 func (c *SafeCounter) Inc(key string){
+=======
+	v  map[string]int
+}
+
+func (c *SafeCounter) Inc(key string) {
+>>>>>>> 3261a91 (push)
 	c.mu.Lock()
 	c.v[key]++
 	c.mu.Unlock()
@@ -23,10 +30,16 @@ func (c *SafeCounter) Value(key string) int {
 	return c.v[key]
 }
 
+<<<<<<< HEAD
 
 func main() {
 	c := SafeCounter{v: make(map[string]int)}
 	for i := 0; i < 1000; i++{
+=======
+func main() {
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+>>>>>>> 3261a91 (push)
 		go c.Inc("somekey")
 	}
 	time.Sleep(time.Second)
