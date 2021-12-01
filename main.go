8a557ff8c00bd9cb0c54d357a493f7f7f712ca4e
941089419@qq.com <941089419@qq.com>
2021-12-01 10:36:53 +0800

    push

--- main.go
+++ main.go
@@ -8,17 +8,11 @@ import (
 
 type SafeCounter struct {
 	mu sync.Mutex
-<<<<<<< HEAD
-	v map[string]int
-}
-
-func (c *SafeCounter) Inc(key string){
-=======
 	v  map[string]int
 }
 
 func (c *SafeCounter) Inc(key string) {
->>>>>>> 3261a91 (push)
+
 	c.mu.Lock()
 	c.v[key]++
 	c.mu.Unlock()
@@ -30,16 +24,9 @@ func (c *SafeCounter) Value(key string) int {
 	return c.v[key]
 }
 
-<<<<<<< HEAD
-
-func main() {
-	c := SafeCounter{v: make(map[string]int)}
-	for i := 0; i < 1000; i++{
-=======
 func main() {
 	c := SafeCounter{v: make(map[string]int)}
 	for i := 0; i < 1000; i++ {
->>>>>>> 3261a91 (push)
 		go c.Inc("somekey")
 	}
 	time.Sleep(time.Second)
